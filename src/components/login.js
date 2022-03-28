ca6617c7e6ef5c2d3121bafe784eea970d89a7a3
Muhammad Alfatih~ <alfatiiihmuh.com>
2022-03-28 23:57:48 +0700

    ver 1

---
+++ src/components/login.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Easy,Fast and Reliable </h1>
+        <p>Go shopping for merchandise, just go to dumb merch shopping. the biggest merchandise in Indonesia</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
